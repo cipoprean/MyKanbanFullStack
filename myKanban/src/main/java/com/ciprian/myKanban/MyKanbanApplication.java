94c7875ee7cbc72a56ddf078d04034fbd1f18e23
cipoprean <coprean92@gmail.com>
2020-08-23 16:16:27 +0300

    first commit

---
+++ myKanban/src/main/java/com/ciprian/myKanban/MyKanbanApplication.java
@@ -0,0 +1,13 @@
+package com.ciprian.myKanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKanbanApplication.class, args);
+	}
+
+}
---
+++ myKanban/src/test/java/com/ciprian/myKanban/MyKanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciprian.myKanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
