81a9b59ab44f84670d6f6747e670de30e5958438
cipoprean <coprean92@gmail.com>
2020-09-07 20:53:50 +0300

    rezolvare conflicte frontend

---
+++ mykanbanclient/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
